1db1b756a63bcf28a1daea876d7a8c22fe5c366e
Nick <nicholasmcdonald40@gmail.com>
2020-04-13 18:57:51 +0200

    Added 2D Julia set visualization / animation example program

---
+++ examples/4_Julia/main.cpp
@@ -0,0 +1,60 @@
+#include "../../TinyEngine.h"
+#include "model.h"
+
+int main( int argc, char* args[] ) {
+
+	Tiny::view.vsync = true;
+
+	Tiny::init("Julia Set Explorer", 1000, 1000);
+
+	Tiny::event.handler = eventHandler;
+	Tiny::view.interface = interfaceFunc;
+
+	//Utility Classes
+	Billboard field(SIZE, SIZE, false); //Render target for automata
+	field.raw(image::make<int>(glm::vec2(SIZE), data, [](int t){
+		return glm::vec4(0.0, 0.0, 0.0, 1.0);
+	}));	//Initialize raw texture data (just an empty billboard)!
+
+	//Shader for drawing billboard to screen and for doing an automata step
+	Shader julia("shader/julia.vs", "shader/julia.fs", {"in_Quad", "in_Tex"});
+
+	//Render Pipeline
+	Tiny::view.pipeline = [&](){
+
+		Tiny::view.target(color::black);
+
+		julia.use();				//Use the Automata Shader
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, field.texture);
+		julia.setInt("imageTexture", 0);
+		julia.setInt("maxiter", res);
+		julia.setFloat("radius", radius);
+		julia.setFloat("zoom", zoom);
+		julia.setInt("order", order);
+		julia.setVec4("converge", glm::vec4(cc[0], cc[1], cc[2], cc[3]));
+		julia.setVec4("diverge", glm::vec4(dc[0], dc[1], dc[2], dc[3]));
+		julia.setVec2("bias", glm::vec2(bias[0], bias[1]));
+		julia.setVec2("offset", glm::vec2(px, py));
+		julia.setMat4("model", field.model);
+
+		field.render();										//Render Field to Self
+
+	};
+
+	Tiny::loop([&](){
+		t += rate;		//Cycle
+		if(t > 1.0) t -= 1.0;
+
+		if(!active) return;	//Pause
+
+		//Animate
+		if(appear) radius = pow(sin(2*PI*t), 2);
+		if(movex) bias[0] = amplitude[0]*sin(2*PI*(t+phase[0]));
+		if(movey) bias[1] = amplitude[1]*sin(2*PI*(t+phase[1]));
+	});
+
+	Tiny::quit();
+
+	return 0;
+}
---
+++ examples/4_Julia/model.h
@@ -0,0 +1,97 @@
+//Data
+const int SIZE = 500;
+int data[SIZE*SIZE] = {0};
+bool paused = true;
+float PI = 3.14159265;
+
+//Resolution
+int res = 25;
+float radius = 2.0;
+float bias[2] = {0.0};
+double zoom = 2.5;
+int order = 2;
+double px = 0.0;
+double py = 0.0;
+
+float cc[4] = {0.070, 0.086, 0.211, 1.0};
+float dc[4] = {0.882, 0.157, 0.204, 1.0};
+
+bool active = false;
+bool appear = false;
+bool movex = false;
+bool movey = false;
+float phase[2] = {0.0, 0.0};
+float amplitude[2] = {1.0, 1.0};
+float rate = 0.01;
+float t = 0.0;
+
+// Event Handler
+std::function<void()> eventHandler = [&](){
+
+  // Scrolling Behavior
+  if(!Tiny::event.scroll.empty()){
+    if(Tiny::event.scroll.back().wheel.y > 0.99){
+      zoom *= 0.9;
+    }
+    else if(Tiny::event.scroll.back().wheel.y < -0.99){
+      zoom /= 0.9;
+    }
+  }
+
+  // Moving Behavior
+  if(!Tiny::event.keys.empty()){
+    if(Tiny::event.keys.back().key.keysym.sym == SDLK_w) py -= 0.1*zoom;
+    else if(Tiny::event.keys.back().key.keysym.sym == SDLK_a) px += 0.1*zoom;
+    else if(Tiny::event.keys.back().key.keysym.sym == SDLK_s) py += 0.1*zoom;
+    else if(Tiny::event.keys.back().key.keysym.sym == SDLK_d) px -= 0.1*zoom;
+  }
+
+};
+
+//Interface Function
+Handle interfaceFunc = [&](){
+  //Window Size
+  ImGui::SetNextWindowSize(ImVec2(480, 260), ImGuiCond_Once);
+  ImGui::SetNextWindowPos(ImVec2(50, 470), ImGuiCond_Once);
+
+  //Open Window
+  ImGui::Begin("Julia Set Controller", NULL, ImGuiWindowFlags_NoResize);
+
+    if(ImGui::BeginTabBar("Tab Bar", ImGuiTabBarFlags_None)){
+      //Current Position Information
+      if(ImGui::BeginTabItem("Info")){
+        ImGui::Text("Position:"); ImGui::SameLine();
+        ImGui::Text("( %f,", px); ImGui::SameLine();
+        ImGui::Text(" %f )", py);
+        ImGui::Text("Zoom: "); ImGui::SameLine();
+        ImGui::Text("%f", zoom);
+        ImGui::EndTabItem();
+      }
+      if(ImGui::BeginTabItem("Control")){
+        ImGui::DragInt("Order", &order, 1, 0, 10);
+        ImGui::DragInt("Resolution", &res, 1, 0, 500);
+        ImGui::DragFloat("Convergence", &radius, 0.01f, 0.0f, 2.0f);
+        ImGui::DragFloat2("Bias", bias, 0.005f, -10.0f, 10.0f);
+        ImGui::EndTabItem();
+      }
+      if(ImGui::BeginTabItem("Visual")){
+        ImGui::ColorEdit3("Converge Color", cc);
+        ImGui::ColorEdit3("Diverge Color", dc);
+        ImGui::EndTabItem();
+      }
+      if(ImGui::BeginTabItem("Animate")){
+        ImGui::DragFloat("Rate", &rate, 0.0001f, 0.001f, 0.01f);
+        ImGui::SameLine(); ImGui::Checkbox("Run", &active);
+        ImGui::Checkbox("Appear", &appear); ImGui::SameLine();
+        ImGui::Checkbox("Move X", &movex); ImGui::SameLine();
+        ImGui::Checkbox("Move Y", &movey);
+        ImGui::DragFloat2("Phase", phase, 0.001f, -1.0f, 1.0f);
+        ImGui::DragFloat2("Amplitude", amplitude, 0.01f, 0.0f, 10.0f);
+
+        ImGui::EndTabItem();
+      }
+      ImGui::EndTabBar();
+    }
+
+  ImGui::End();
+};

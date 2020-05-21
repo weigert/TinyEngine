bb31087ebddfce9560ddd44ea51ed56debc22fb3
Nick <nicholasmcdonald40@gmail.com>
2020-05-21 18:20:33 +0200

    Added Signed Distance Field and Raymarching Examples

--- examples/4_Julia/main.cpp
+++ examples/4_Julia/main.cpp
@@ -6,19 +6,19 @@ int main( int argc, char* args[] ) {
 
 	Tiny::view.vsync = true;
 
-	Tiny::window("Julia Set Explorer", 1000, 1000);
+	Tiny::window("Julia Set Explorer", 1000, 1000);	//Open Window
 
-	Tiny::event.handler  = eventHandler;
-	Tiny::view.interface = interfaceFunc;
+	Tiny::event.handler  = eventHandler;	//Set Event Handler
+	Tiny::view.interface = interfaceFunc;	//Set Interface Function
 
 	Square2D flat;												//Flat geometry primitive
 	Shader julia({"shader/julia.vs", "shader/julia.fs"}, {"in_Quad", "in_Tex"});
 
 	Tiny::view.pipeline = [&](){
 
-		Tiny::view.target(color::black);	//Target Screen
+		Tiny::view.target(color::black);		//Target Screen
 
-		julia.use();
+		julia.use();												//Prepare Shader
 		julia.uniform("maxiter", res);
 		julia.uniform("radius", radius);
 		julia.uniform("zoom", zoom);
@@ -28,11 +28,11 @@ int main( int argc, char* args[] ) {
 		julia.uniform("bias", glm::vec2(bias[0], bias[1]));
 		julia.uniform("offset", glm::vec2(px, py));
 		julia.uniform("model", flat.model);
-		flat.render();
+		flat.render();											//Draw onto Flat Geometry
 
 	};
 
-	Tiny::loop([&](){
+	Tiny::loop([&](){											//Extra Stuff (every tick)
 		t += rate;		//Cycle
 		if(t > 1.0) t -= 1.0;
 

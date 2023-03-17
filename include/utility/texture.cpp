a2bb7e342056074c4f191d2cbbf7f8f672b4ce68
Nick <nicholasmcdonald40@gmail.com>
2023-03-17 16:52:29 +0100

    restructuring texture

--- include/utility/texture.cpp
+++ include/utility/texture.cpp
@@ -31,6 +31,17 @@ public:
 
 };
 
+void Texture::setup(TextureConfig TC, void* data){
+  glBindTexture( type, texture );
+  glTexImage2D( type, 0, TC.internal, W, H, 0, TC.format, TC.type, data );
+  Texture::parameter(this);
+}
+
+void Texture::raw(SDL_Surface* s){  //Generate a texture from raw surface data
+  setup({GL_RGBA, GL_RGBA, GL_UNSIGNED_BYTE}, s->pixels);
+  SDL_FreeSurface(s);
+}
+
 class Cubetexture: public Texture{  //Cubetexture specialization.
 public:                             //Same thing, 6 times
   Cubetexture():Texture(){ type = GL_TEXTURE_CUBE_MAP; };
@@ -46,45 +57,6 @@ public:                             //Same thing, 6 times
   void setup(TextureConfig TC, void* data = NULL);
 };
 
-void Texture::empty(int W, int H, tfunc param, GLenum F){
-  glBindTexture( type, texture );
-  glTexImage2D(type, 0, F, W, H, 0, F, GL_UNSIGNED_BYTE, NULL);
-  (param)(this);              //Call Parameter Setter
-}
-
-void Texture::depth(int W, int H, tfunc param){
-  empty(W, H, param, GL_DEPTH_COMPONENT);
-}
-
-void Texture::raw(SDL_Surface* s, tfunc param){  //Generate a texture from raw surface data
-
-  glBindTexture( type, texture );
-
-  /*
-  //   glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
-  glActiveTexture(GL_TEXTURE0 + boundtextures);
-
-
-          {
-      GLenum err = glGetError();
-      if(err != GL_NO_ERROR){
-        std::cout<<err<<std::endl;
-      }
-      else std::cout<<"AYY4"<<std::endl;
-    }
-    */
-
-  glTexImage2D(type, 0, GL_RGBA, s->w, s->h, 0, GL_RGBA, GL_UNSIGNED_BYTE, s->pixels);
-
-
-
-  (param)(this); //Call the parameter setting function!
-  SDL_FreeSurface(s);
-
-
-
-}
-
 //Default parameter setting function!
 //Note that you can pass your own to the texture building functions above!z
 tfunc Texture::parameter = [](Texture* t){
@@ -93,17 +65,6 @@ tfunc Texture::parameter = [](Texture* t){
   glTexParameteri(t->type, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
   glTexParameteri(t->type, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
   glTexParameteri(t->type, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-};
-
-void Cubetexture::empty(int W, int H, tfunc param, GLenum F){
-  glBindTexture(type, texture);
-  for(unsigned int i = 0; i < 6; i++)
-    glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, F, W, H, 0, F, GL_UNSIGNED_BYTE, NULL);
-  (param)(this);
-}
-
-void Cubetexture::depth(int W, int H, tfunc param){
-  empty(W, H, param, GL_DEPTH_COMPONENT);
 };
 
 #endif
